39f6f044075580e382c98ecf898fd329fc31f265
SpaceCat <missbodre@gmail.com>
2018-05-01 18:32:24 +1000

    Node basics

---
+++ ZC_Node/server.js
@@ -0,0 +1,15 @@
+// const http = require('http'); 
+// http.createServer((request, response) => { 
+//   console.log('Starting server');
+// }).listen(80);
+
+function helloWorld() {
+  console.log('Hello World');
+  helloNode();
+}
+
+function helloNode() {
+  console.log('Hello Node');
+}
+
+helloWorld();
